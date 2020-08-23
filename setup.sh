0f29a078ee295905a4860d4f8e9f039251798165
Benjamin Sautner <1295207+bsautner@users.noreply.github.com>
2020-08-23 16:06:45 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudu apt dist-upgrade -y
 sudu apt autoremove -y
 sudu apt clean
 
-sudo apt install -y preload git
+sudo apt install -y preload git net-tools
 
 ssh-keygen -t rsa -b 4096 -C "$USER@sautner.me"
 sudo reboot
