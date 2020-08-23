5c07a791aa154e47c315192c01cc23c20503b75f
Benjamin Sautner <1295207+bsautner@users.noreply.github.com>
2020-08-23 15:39:11 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-apt update 
-apt upgrade -y
-apt autoremove
-apt clean
+sudu apt update 
+sudu apt upgrade -y
+sudu apt dist-upgrade -y
+sudu apt autoremove -y
+sudu apt clean
+
+
+ssh-keygen -t rsa -b 4096 -C "$USER@sautner.me"
+sudo reboot
