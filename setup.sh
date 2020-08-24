c75d8b60e930c69d8792c617ccf8bd6ad70e172b
Benjamin Sautner <1295207+bsautner@users.noreply.github.com>
2020-08-24 13:05:15 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,12 +8,13 @@ sudu apt dist-upgrade -y
 sudu apt autoremove -y
 sudu apt clean
 
-sudo apt-get remove android-sdk
-sudo apt-get autoremove android-sdk
-sudo apt-get purge android-sdk
-sudo apt-get autoremove --purge android-sdk
+sudo add-apt-repository ppa:mozillateam/firefox-next
 
-sudo apt install -y preload git net-tools openjdk-8-jdk android-sdk snapd
+wget -q -O - https://www.charlesproxy.com/packages/apt/PublicKey | sudo apt-key add -
+
+
+sudo apt install -y preload git net-tools openjdk-8-jdk android-sdk snapd firefox charles-proxy
+
 sudo cp ./environment /etc/environment
 source /etc/environment
 sudo chown -R $USER $ANDROID_HOME 
@@ -23,11 +24,9 @@ cd $ANDROID_HOME/cmdline-tools
 unzip cmdline-tools.zip
 rm cmdline-tools.zip
 sudo snap install android-studio --classic
+ 
 
-
-wget -q -O - https://www.charlesproxy.com/packages/apt/PublicKey | sudo apt-key add -
-sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 1AD28806
-sudo apt-get update
-sudo apt-get install charles-proxy -y
-
-sudo reboot
+#sudo apt-get remove android-sdk
+#sudo apt-get autoremove android-sdk
+#sudo apt-get purge android-sdk
+#sudo apt-get autoremove --purge android-sdk
