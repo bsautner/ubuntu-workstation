a8f6ec9a345d2598db7ff4cdbaba1fc7be9bef96
Benjamin Sautner <1295207+bsautner@users.noreply.github.com>
2020-08-24 09:38:19 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ sudu apt autoremove -y
 sudu apt clean
 
 sudo apt install -y preload git net-tools openjdk-8-jdk android-sdk
+sudo cp ./environment /etc/environment
+source /etc/environment
 
 ssh-keygen -t rsa -b 4096 -C "$USER@sautner.me"
 sudo reboot
