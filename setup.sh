e9cb48f869802a13246c2d21c0259342f4167804
Benjamin Sautner <1295207+bsautner@users.noreply.github.com>
2020-09-02 12:12:36 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -43,6 +43,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker 
 
 #sudo apt-get remove android-sdk
 #sudo apt-get autoremove android-sdk
