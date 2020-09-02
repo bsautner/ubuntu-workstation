19b02f3f37b0e886e7f8cc72f73865ae835844b6
Benjamin Sautner <1295207+bsautner@users.noreply.github.com>
2020-09-02 12:05:02 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -36,6 +36,13 @@ sudo snap install android-studio --classic
 git config --global user.email "$USER@sautner.me"
 git config --global user.name "$USER Sautner"
 
+#docker
+
+sudo apt-get install  apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 #sudo apt-get remove android-sdk
 #sudo apt-get autoremove android-sdk
