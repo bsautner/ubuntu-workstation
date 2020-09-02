248dc205be25758e08967281c0a5d0c12d3366d2
Benjamin Sautner <1295207+bsautner@users.noreply.github.com>
2020-09-02 12:19:08 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -37,14 +37,14 @@ git config --global user.email "$USER@sautner.me"
 git config --global user.name "$USER Sautner"
 
 #docker
-
+sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get install  apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
-sudo usermod -aG docker 
+sudo usermod -aG docker $USER
 
 #sudo apt-get remove android-sdk
 #sudo apt-get autoremove android-sdk
